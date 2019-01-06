725f026e34480a9d491988910a8a4fbd83b70b2c
AdrianRybandt <adrian.rybandt@gmail.com>
2019-01-06 20:09:58 +0100

    puting in order

---
+++ lib/Game.rb
@@ -0,0 +1,21 @@
+#Gameboard-plansza gry, w pliku,który można załadować
+#Players-klasa
+#Leaderboard-plik, lista nazwa.gracz ilośćzwyciestw najwyzszywynik
+#TileBag-tabelka z ilośćią klocków, wartością punktową
+
+class Game
+
+def setupplayers
+puts "How many players?"
+nrofplayers = get_int_input
+#addplayers po podaniu imion nrofplayersrazy
+end
+
+def addplayer(nick)
+#gdzies check czy juz nie istnieje
+@nick=nick
+#zapiszdoplikuleaderboards
+end
+
+
+
