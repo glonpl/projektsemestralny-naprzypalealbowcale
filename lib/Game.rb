8e1ca55438417b1b4227b981aca094e980106636
glonpl <bratajczyk@o2.pl>
2019-01-06 21:28:05 +0100

    jest cienszko

--- lib/Game.rb
+++ lib/Game.rb
@@ -2,8 +2,21 @@
 #Players-klasa
 #Leaderboard-plik, lista nazwa.gracz ilośćzwyciestw najwyzszywynik
 #TileBag-tabelka z ilośćią klocków, wartością punktową
- 
+
 class Game
+
+def pointstable{
+
+  "A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4, "G"=>2, "H"=>4, "I"=>1, "J"=>8,
+  "K"=>5, "L"=>1, "M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+  "U"=>1, "V"=>4, "W"=>4, "X"=>8, "Y"=>4, "Z"=>10
+
+ }
+
+end
+
+
+
 	def setupplayers
 	 puts "How many players?"
 	 nrofplayers = get_int_input
@@ -19,11 +32,9 @@ class Game
 return players
 end
 end
- 
+
 def addplayer(nick)
 #gdzies check czy juz nie istnieje
 @nick=nick
 #zapiszdoplikuleaderboards
 end
-
-
