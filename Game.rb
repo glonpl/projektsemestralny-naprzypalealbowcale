ebf596e8b68c7f19a9b75396047b7d8faaaef735
Bartosz Ratajczyk <bratajczyk@sigma.ug.edu.pl>
2019-01-06 19:32:49 +0100

    pseudokod

---
+++ Game.rb
@@ -0,0 +1,14 @@
+#Gameboard-plansza gry, w pliku,który można załadować
+#Players-klasa
+#Leaderboard-plik, lista nazwa.gracz ilośćzwyciestw najwyzszywynik
+#TileBag-tabelka z ilośćią klocków, wartością punktową
+
+class Player
+def add(nick)
+#gdzies check czy juz nie istnieje
+@nick=nick
+#zapiszdoplikuleaderboards
+end
+
+
+
