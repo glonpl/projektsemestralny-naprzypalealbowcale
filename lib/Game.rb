670a084ceedc68b05c02d661e767f8bfc62d1131
AdrianRybandt <adrian.rybandt@gmail.com>
2019-01-06 20:35:29 +0100

    Jesus

--- lib/Game.rb
+++ lib/Game.rb
@@ -2,20 +2,28 @@
 #Players-klasa
 #Leaderboard-plik, lista nazwa.gracz ilośćzwyciestw najwyzszywynik
 #TileBag-tabelka z ilośćią klocków, wartością punktową
-
+ 
 class Game
+	def setupplayers
+	 puts "How many players?"
+	 nrofplayers = get_int_input
 
-def setupplayers
-puts "How many players?"
-nrofplayers = get_int_input
 #addplayers po podaniu imion nrofplayersrazy
+	 players = []
+	 puts "Type in player names:"
+      	 nrofplayers.times do |player|
+	 	puts "What is the \##{player + 1} player name?"
+	 	gamer=gets.chomp
+		addplayer(gamer)
+	 end
+return players
 end
-
+end
+ 
 def addplayer(nick)
 #gdzies check czy juz nie istnieje
 @nick=nick
 #zapiszdoplikuleaderboards
 end
-
 
 
