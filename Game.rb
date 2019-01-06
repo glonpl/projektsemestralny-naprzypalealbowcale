eede373a4caf93af76fc10ff79f120e9c006a799
Bartosz Ratajczyk <bratajczyk@sigma.ug.edu.pl>
2019-01-06 19:42:24 +0100

    pseudokod

--- Game.rb
+++ Game.rb
@@ -3,8 +3,15 @@
 #Leaderboard-plik, lista nazwa.gracz ilośćzwyciestw najwyzszywynik
 #TileBag-tabelka z ilośćią klocków, wartością punktową
 
-class Player
-def add(nick)
+class Game
+
+def setupplayers
+puts "How many players?"
+nrofplayers = get_int_input
+#addplayers po podaniu imion nrofplayersrazy
+end
+
+def addplayer(nick)
 #gdzies check czy juz nie istnieje
 @nick=nick
 #zapiszdoplikuleaderboards
